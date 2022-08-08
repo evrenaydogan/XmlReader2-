5f4cd24b783091082c03373d43c7d49bfa453bb5
Burak Hamzaoğlu <burakh16@gmail.com>
2022-08-08 12:53:44 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace XmlReader2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton5 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton6 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton7 = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(23, 21);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Örnek Çalışma";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(23, 62);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton2.TabIndex = 1;
+            this.simpleButton2.Text = "XmlRead Kısa";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Location = new System.Drawing.Point(23, 110);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(157, 23);
+            this.simpleButton3.TabIndex = 2;
+            this.simpleButton3.Text = "Foreach / Projects Properties";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            // 
+            // simpleButton4
+            // 
+            this.simpleButton4.Location = new System.Drawing.Point(23, 152);
+            this.simpleButton4.Name = "simpleButton4";
+            this.simpleButton4.Size = new System.Drawing.Size(157, 23);
+            this.simpleButton4.TabIndex = 3;
+            this.simpleButton4.Text = "Foreach / XML Child Node";
+            this.simpleButton4.Click += new System.EventHandler(this.simpleButton4_Click);
+            // 
+            // simpleButton5
+            // 
+            this.simpleButton5.Location = new System.Drawing.Point(252, 21);
+            this.simpleButton5.Name = "simpleButton5";
+            this.simpleButton5.Size = new System.Drawing.Size(157, 23);
+            this.simpleButton5.TabIndex = 4;
+            this.simpleButton5.Text = "Foreach / XML Child Node";
+            this.simpleButton5.Click += new System.EventHandler(this.simpleButton5_Click);
+            // 
+            // simpleButton6
+            // 
+            this.simpleButton6.Location = new System.Drawing.Point(252, 62);
+            this.simpleButton6.Name = "simpleButton6";
+            this.simpleButton6.Size = new System.Drawing.Size(157, 23);
+            this.simpleButton6.TabIndex = 5;
+            this.simpleButton6.Text = "Foreach / XML Child Node";
+            this.simpleButton6.Click += new System.EventHandler(this.simpleButton6_Click);
+            // 
+            // simpleButton7
+            // 
+            this.simpleButton7.Location = new System.Drawing.Point(480, 21);
+            this.simpleButton7.Name = "simpleButton7";
+            this.simpleButton7.Size = new System.Drawing.Size(157, 23);
+            this.simpleButton7.TabIndex = 6;
+            this.simpleButton7.Text = "Foreach / XML Child Node";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.simpleButton7);
+            this.Controls.Add(this.simpleButton6);
+            this.Controls.Add(this.simpleButton5);
+            this.Controls.Add(this.simpleButton4);
+            this.Controls.Add(this.simpleButton3);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton4;
+        private DevExpress.XtraEditors.SimpleButton simpleButton5;
+        private DevExpress.XtraEditors.SimpleButton simpleButton6;
+        private DevExpress.XtraEditors.SimpleButton simpleButton7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XmlReader2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            var path = @"D:\Deneme.xml";
+            List<Details> detailsList = new List<Details>();
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            XmlNodeList nodeList = doc.SelectNodes("/Invoice/InvoiceHeader");
+            foreach (XmlNode node in nodeList)
+            {
+                // create details class for each InvoiceHeader
+                Details detail = new Details();
+                detail.Number = new List<string>();
+
+                // loop over child nodes to get Name and all Number elements
+                foreach (XmlNode child in node.ChildNodes)
+                {
+                    // check node name to decide how to handle the values               
+                    if (child.Name == "Name")
+                    {
+                        detail.Name = child.InnerText;
+                    }
+                    else if (child.Name == "Number")
+                    {
+                        detail.Number.Add(child.InnerText);
+                    }
+                }
+                detailsList.Add(detail);
+            }
+        }
+
+        public class Details
+        {
+            public string Name { get; set; }
+            public List<string> Number { get; set; }
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            //    var path = @"C:\Users\BitegKullanici1\Desktop\Adeko XML Örnek ve Açıklamaları\Örnek 1\1-Modul_Kisa_Doors.xml";
+            //    List<Models.Doors> doorsList = new List<Models.Doors>();
+            //    XmlDocument doc = new XmlDocument();
+            //    doc.Load(path);
+            //    XmlNodeList nodeList = doc.SelectNodes("/PROJECT/DOORS");
+            //    foreach (XmlNode node in nodeList)
+            //    {
+            //        // create details class for each InvoiceHeader
+            //        Models.Doors doors = new Models.Doors();
+
+
+            //        // loop over child nodes to get Name and all Number elements
+            //        foreach (XmlNode child in node.ChildNodes)
+            //        {
+            //            // check node name to decide how to handle the values               
+            //            if (child.Name == "GROUP")
+            //            {
+            //                doors.Group = child.InnerText;
+            //            }
+            //            else if (child.Name == "CODE")
+            //            {
+            //                doors.Code = child.InnerText;
+            //            }
+            //            else if (child.Name == "TYPE")
+            //            {
+            //                doors.Type = child.InnerText;
+            //            }
+            //            else if (child.Name == "MODEL")
+            //            {
+            //                doors.Model = child.InnerText;
+            //            }
+            //            else if (child.Name == "MATERIAL")
+            //            {
+            //                doors.Material = child.InnerText;
+            //            }
+            //            else if (child.Name == "DIRECTION")
+            //            {
+            //                doors.Direction = child.InnerText;
+            //            }
+            //            else if (child.Name == "OPENDIRECTION")
+            //            {
+            //                doors.OpenDirection = child.InnerText;
+            //            }
+            //            else if (child.Name == "MODULPOSE" && child.InnerText != "")
+            //            {
+            //                doors.ModulePose = Convert.ToInt32(child.InnerText);
+            //            }
+            //            else if (child.Name == "BRAND")
+            //            {
+            //                doors.Brand = child.InnerText;
+            //            }
+            //            else if (child.Name == "MODUL")
+            //            {
+            //                doors.Modul = child.InnerText;
+            //            }
+            //            else if (child.Name == "H" && child.InnerText != "")
+            //            {
+            //                doors.H = Convert.ToInt32(child.InnerText);
+            //            }
+            //            else if (child.Name == "W" && child.InnerText != "")
+            //            {
+            //                doors.W = Convert.ToInt32(child.InnerText);
+            //            }
+            //            else if (child.Name == "MATROTA" && child.InnerText != "")
+            //            {
+            //                doors.MatRota = Convert.ToInt32(child.InnerText);
+            //            }
+            //            else if (child.Name == "QUANTITY" && child.InnerText != "")
+            //            {
+            //                doors.Quantity = Convert.ToDecimal(child.InnerText);
+            //            }
+
+            //        }
+            //        doorsList.Add(doors);
+            //}
+
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            Models.Project project = new Models.Project();
+
+            foreach (var prop in project.GetType().GetProperties())
+            {
+                MessageBox.Show(prop.Name.ToString());
+            }
+
+        }
+
+        private void simpleButton4_Click(object sender, EventArgs e)
+        {
+            var path = @"C:\Users\BitegKullanici1\Desktop\Adeko XML Örnek ve Açıklamaları\Örnek 1\1-Modul.xml";
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            XmlNodeList nodeList = doc.SelectNodes("/PROJECT/*");
+        }
+
+        private void simpleButton5_Click(object sender, EventArgs e)
+        {
+            var path = @"C:\Users\BitegKullanici1\Desktop\Adeko XML Örnek ve Açıklamaları\Örnek 1\Deneme.xml";
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Models.Product>), new XmlRootAttribute("Products"));
+            StreamReader stringReader = new StreamReader(path);
+
+            List<Models.Product> productList = (List<Models.Product>)serializer.Deserialize(stringReader);
+
+        }
+
+        private void simpleButton6_Click(object sender, EventArgs e)
+        {
+            //var path = @"C:\Users\BitegKullanici1\Desktop\Adeko XML Örnek ve Açıklamaları\Örnek 1\1-Modul_Kisa_Doors.xml";
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Models.Doors3>), new XmlRootAttribute("PROJECTS"));
+            StreamReader streamReader = new StreamReader(@"C:\Users\BitegKullanici1\Desktop\Adeko XML Örnek ve Açıklamaları\Örnek 1\Deneme4.xml");
+
+            List<Models.Doors3> doorslist = (List<Models.Doors3>)serializer.Deserialize(streamReader);
+        }
+    }
+}
